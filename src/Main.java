a0659cec16eacc0afe2da6ca8e4d2c9f37af7741
Ronny <ronny761@gmail.com>
2024-10-29 12:42:14 +0200

    template

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int num = 45; // מספר לבדיקה
+        boolean isKaprekar = isKaprekar(num);
+
+        // הצגת התוצאה
+        System.out.println("Is " + num + " a Kaprekar number? " + isKaprekar);
+    }
+
+    // פעולה לבדיקת מספר קפרקר
+    public static boolean isKaprekar(int num) {
+        // TODO: השלם את הלוגיקה לבדיקת מספר קפרקר
+        return false; // החזרה זמנית
+    }
+}
